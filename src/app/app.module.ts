2e37401aa666bd0ceec3e1c8d509d0b81d1793ea
Jeff Winget <wingej0@pop-os.localdomain>
2020-06-03 10:53:15 -0600

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Created module declarations
+import { CoreModule } from './core/core.module';
+import { ModulesModule } from './modules/modules.module';
+import { SharedModule } from './shared/shared.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    ModulesModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
