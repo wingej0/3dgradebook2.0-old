596f7fedc440876930fbf85a0f1f3796dfa88f3a
Jeff Winget <wingej0@pop-os.localdomain>
2020-06-26 10:10:09 -0600

    Updated database rules and firebase settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB8y-D2h6QAS6EcuvhgzI9PkfUrL0QnTjU",
+    authDomain: "sbgradebook-b9551.firebaseapp.com",
+    databaseURL: "https://sbgradebook-b9551.firebaseio.com",
+    projectId: "sbgradebook-b9551",
+    storageBucket: "sbgradebook-b9551.appspot.com",
+    messagingSenderId: "179271736820",
+    appId: "1:179271736820:web:ba6ad98b8914a6dded2da1",
+    measurementId: "G-XR5SLZR6YS"
+  }
 };
