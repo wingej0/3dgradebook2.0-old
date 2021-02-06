467c9a76dd8a5745b58f1b745d068d8a3c3f4d4d
Jeff Winget <wingej0@pop-os.localdomain>
2021-02-06 14:38:07 -0700

    updated to Angular 11

--- src/app/modules/courses/canvas-courses/canvas-courses.component.spec.ts
+++ src/app/modules/courses/canvas-courses/canvas-courses.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CanvasCoursesComponent } from './canvas-courses.component';
 
@@ -6,7 +6,7 @@ describe('CanvasCoursesComponent', () => {
   let component: CanvasCoursesComponent;
   let fixture: ComponentFixture<CanvasCoursesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CanvasCoursesComponent ]
     })
--- src/app/modules/standards/standards/standards.component.spec.ts
+++ src/app/modules/standards/standards/standards.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { StandardsComponent } from './standards.component';
 
@@ -6,7 +6,7 @@ describe('StandardsComponent', () => {
   let component: StandardsComponent;
   let fixture: ComponentFixture<StandardsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ StandardsComponent ]
     })
